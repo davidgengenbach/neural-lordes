b7cc5dc14541e00cb4483f56fb95a2d9bd562ffa
David Gengenbach <davidgengenbach@gmx.de>
2016-07-25 18:31:19 +0200

    :)

--- perceptron.py
+++ perceptron.py
@@ -1,116 +1,142 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import random
 
-
 class Perceptron:
-    def __init__(self, inpsize, id, activation_function):
-        self.weights = [random.uniform(-1, 1) for x in range(inpsize)]
-        self.bias = 0
-        self.id = id
-        self.inactive = False
-        self.activation_function = activation_function
+    def __init__(self, inpsize,id):
+        self.weights = [random.uniform(-3, 3) for x in range(inpsize)]
+        self.bias=0
+        self.id=id
+        self.inactive=False
 
-    def activationfunction_der(self, x):
-        #return 1 - np.tanh(x)*np.tanh(x)
-        h = 0.000000000001
-        return (self.activation_function(x+h) - self.activation_function(x))/h
+    def activationfunction(self,x):
+        #return (1/(1*np.sqrt(2*np.pi))*np.exp(-0.5*np.power(x/1,2))) #gauss
+        #return 1 - np.power((np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x)), 2) #tanh der
+        return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x)) #tanh
 
-    def propagate(self, inp):
-        self.input = inp
+    def activationfunction_der(self,x):
+        h=0.000000001
+        return (self.activationfunction(x+h)-self.activationfunction(x))/h
 
+        #return 1-np.power(self.activationfunction(x),2)
+
+    def propagate(self,inp):
+        self.input=inp
+        self.output=self.activationfunction(np.dot(inp, self.weights)+self.bias)
         if self.id == True:
-            return inp[0]
+            self.output=inp[0]
 
-        if self.inactive == True:
-            return 0
+        if self.inactive==True:
+            self.output=0
 
-        return self.activation_function(np.dot(inp, self.weights) + self.bias)
-
-    def learn(self, optimal_out, alpha, errorterm = None):
-        if errorterm == None:
-            errorterm = self.output - optimal_out
-        out_der = self.activationfunction_der(np.dot(self.input, self.weights) + self.bias)
-        self.delta = out_der * errorterm
-        self.weights = self.weights - alpha * self.delta*np.transpose(self.input)
-        self.bias=self.bias - alpha * self.delta
+        return self.output
 
 
-def tanh_af(x):
-        return np.tanh(x)
-
-
-
-mongo = Perceptron(1, False, tanh_af)
-idiot = Perceptron(1, False, tanh_af)
-
-
-
-
-
-
-
-def linear_training_data(m, b):
-    for x in range (100):
-        yield (x, m*x + b)
-
-data = list(linear_training_data(-1,1))
-
-
-
-
-
-for x,y in data:
-    mongo.output = mongo.propagate([x])
-    mongo.learn(y, 0.2)
-
-
-
-print mongo.output
-
-
-
-#plt.scatter([1],[1])
-#plot.show()
-
-
-
+    def learn(self,opimal_out,alpha,errorterm):
+        if errorterm==None:
+            errorterm = self.output-opimal_out
+        out_der = self.activationfunction_der(np.dot(self.input, self.weights)+self.bias)
+        self.delta =  out_der * errorterm
+        self.weights = self.weights - alpha*self.delta*np.transpose(self.input)
+        self.bias=self.bias-alpha*self.delta
 
 class ffnet:
-    def __init__(self, inpsize, hiddenlayers, hiddenlayerheight):
+    def __init__(self, inpsize,outpsize,hiddenlayers,hiddenlayerheight):
         self.layers = []
-
-        self.layers.append([Perceptron(1, True) for x in range(inpsize)])
+        #input layer
+        self.layers.append([Perceptron(1,True) for x in range(inpsize)])
         nextinpsize=inpsize
 
         for l in range(hiddenlayers):
-            #hidden layer
-            self.layers.append([Perceptron(nextinpsize, False) for x in ringe (outpsize)])
-            nextinpsize = hiddenlayerheight
+            # hidden layer
+            self.layers.append([Perceptron(nextinpsize,False) for x in range(hiddenlayerheight)])
+            nextinpsize=hiddenlayerheight
 
-        #output layer
-        self.layers.append([Perceptron(nextinpsize, False) for x in range(outpsize)])
+        # output layer
+        self.layers.append([Perceptron(nextinpsize,False) for x in range(outpsize)])
 
-    def get_layer_output(self, layer):
+    def get_layer_output(self,layer):
         return [self.layers[layer][p].output for p in range(len(self.layers[layer]))]
 
-    def propagate(self, inp):
+    def propagate(self,inp):
         for l in range(len(self.layers)):
             for p in range(len(self.layers[l])):
                 self.layers[l][p].propagate(inp)
-            inp = self.get_layer_output(l)
+            inp=self.get_layer_output(l)
         return inp
 
-    def learn(self, optimal_out, alpha):
+
+
+    def learn(self,opimal_out,alpha):
         for lx in range(len(self.layers)):
-            l = len(self.layers)-lx-1
+            l=len(self.layers)-lx-1
             for p in range(len(self.layers[l])):
                 perceptron = self.layers[l][p]
                 if lx == 0:
-                    perceptron.learn(optimal_out[p], alpha, None)
+                    perceptron.learn(opimal_out[p], alpha, None)#
                 else:
                     trainoutp = 0
                     for p2 in range(len(self.layers[l+1])):
-                        tainoutp = trainoutp + self.layers[l+1][p2].delta * self.layers[l+1][p2].weights[p]
-
+                        trainoutp=trainoutp+self.layers[l+1][p2].delta*self.layers[l+1][p2].weights[p]
                     perceptron.learn(None, alpha, trainoutp)
+
+
+
+
+#in_data=(1.0)
+#out_data=(0.7)
+
+net=ffnet(1,1,2,10)
+
+neuron=Perceptron(1,False)
+
+for i in range(100):
+    print net.propagate([1])
+    net.learn([0.5],0.5)
+
+def target(x):
+    #return np.power(x,1.1)
+    return np.sin(x*8)
+
+def netf(x):
+    return net.propagate([x])
+
+def netfvec(x):
+    return [net.propagate([xi]) for xi in x]
+
+r=np.arange(0, 1, 0.05)
+#r2=np.arange(-1, 2, 0.05)
+plt.plot(r, target(r))
+
+for i in range(200):
+    #print i
+    alpha=0.01#random.uniform(0.000001, 0.01)
+    #net.randomshutoff(5)
+    rran = random.sample(r, len(r))
+
+    for x in rran:
+        ty=target(x)
+        y = net.propagate([x])
+        net.learn([ty], alpha)#0.001
+        #plt.scatter([x], [y])
+
+    if i%10==0:
+    #    print netfvec(r)
+        plt.plot(r, netfvec(r), color='green')
+
+    #for x in r:
+    #    plt.scatter([x], net.propagate([x]),color='green')
+        #for xt in range(100):
+        #    plt.scatter([xt], net.propagate([xt]))
+        #plt.scatter([x], [y], color='red')
+
+       #print neuron.propagate(in_data)
+
+#net.randomshutoff(0)
+plt.plot(r, netfvec(r), color='red')
+#plt.plot(r2, netfvec(r2), color='red')
+#plt.plot(r2, target(r2), color='blue')
+#plt.scatter([1],[1])
+#plt.scatter([2],[2])
+#plt.plot([1,2,3,4])
+plt.show()
