97febba19f48040785cf2fda33f5438b16ceab43
patrickwieth <patrick.wieth@googlemail.com>
2016-08-01 21:50:50 +0200

    sinus fitting works

--- neuron-keintroll.py
+++ neuron-keintroll.py
@@ -1,11 +1,12 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import random 
 
 
 
-def setup_weights(layer_number, layer_size):
-	weights = np.random.rand(layer_number, layer_size)
+def setup_weights(layer_height, layer_size):
+	weights = np.random.rand(layer_height, layer_size)
+	weights = (weights - 0.5)/10
 	return weights
 
 def apply_activation_fns(activation_function, input_data, weights):
@@ -21,25 +22,26 @@ def error_fn2(weights, deltas):
 	return np.dot(weights, deltas)
 
 def learn_fn(derivative_activation_fn, deltas, input_data, weights):
-	alpha = 0.2
+	alpha = 0.1
 	#return np.outer(deltas, input_data)
 	return weights - alpha * np.outer(input_data, deltas)
-
 
 def sech(x):
 	return 1/np.cosh(x)**2
 
 
-
-def train_feed_forward(layer_list, training_data):
-	act_fn = np.tanh
-	act_fn_derived = sech
-
+def init_feed_forward(layer_list):
 	weight_list = []
 
 	for i in range(len(layer_list)-1):
 		weight_list.append(setup_weights(layer_list[i], layer_list[i+1]))
 
+	return weight_list
+
+	
+def train_feed_forward(weight_list, training_data):
+	act_fn = np.tanh
+	act_fn_derived = sech
 
 	for i in range(len(training_data)):
 
@@ -90,38 +92,82 @@ def apply_network(weights, input):
 
 
 
+def normalize_data(data):
+	x_max = 0
+
+	for d in data:
+		if(abs(d[0][0]) > x_max):
+			x_max = abs(d[0][0])
+
+	y_max = 0
+
+	for d in data:
+		if(abs(d[1]) > y_max):
+			y_max = abs(d[1])
+
+	for i in range(len(data)):
+		data[i] = [data[i][0] / x_max, data[i][1] / y_max]
+
+	return [x_max, y_max, data]
 
 
-def absolute_training_data():
-	training_data = np.tile(np.array([[1,2,3,4], 0.5]), (100, 1) )
+def renormalize_data(data, factors):
+	for i in range(len(data)):
+		data[i] = [data[i][0] * factors[0], data[i][1] * factors[1]]
 
-	# add random noise
-	for i in range(len(training_data)):
-		training_data[i, 0] = training_data[i,0] + 0.2 * np.random.rand(4)
-
-	return training_data
+	return data
 
 
 
 # sinusoidal training data
-sin_data = np.array([[[x], np.sin(x)] for x in np.arange(-3.14, 3.14, 0.1)])
+rangex1 = np.arange(-6, 6, 0.1)
+rangex = random.sample(rangex1, len(rangex1))
+sin_data = np.array([[[x], np.sin(x)] for x in rangex ])
+	
+
+training_data = normalize_data(sin_data)
+
+stretch_factors = [training_data[0], training_data[1]]
+
+sin_data = renormalize_data(sin_data, stretch_factors)
+
+print stretch_factors
+
+training_data = training_data[2]
+
+print training_data
 
 
-training_data = sin_data
-#training_data = absolute_training_data()
 
-#print training_data
+start_weights = init_feed_forward([1,10,10,1])
+network = train_feed_forward(start_weights, training_data);
 
+for x in range(1000):
+	network = train_feed_forward(network, training_data);	
 
-
-network = train_feed_forward([1,10,10,1], training_data)
-print network
-
-result = apply_network(network, 0)
+#print network
 
 
-print "result", result
+result = [apply_network(network, x)[0,0] for x in rangex1 ]
 
+#print "result", [rangex, result]
+
+
+xy_data = zip(rangex1, result)
+
+stretch_factors[0] = 1
+xy_data = renormalize_data(xy_data, stretch_factors)
+
+#print xy_data
+
+for x in xy_data:
+	plt.scatter(x[0], x[1])
+
+for x in sin_data:
+	plt.scatter(x[0], x[1], color='red')
+
+#plt.plot(xy_data[0], xy_data[1])
+plt.show()
 
 
 '''
@@ -133,3 +179,12 @@ data = list(linear_training_data(-1,1))
 '''
 
 
+
+def absolute_training_data():
+	training_data = np.tile(np.array([[1,2,3,4], 0.5]), (100, 1) )
+
+	# add random noise
+	for i in range(len(training_data)):
+		training_data[i, 0] = training_data[i,0] + 0.2 * np.random.rand(4)
+
+	return training_data
